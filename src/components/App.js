1b35e68378e0a8f4d41c00d45249f482b0622877
Imtiaz Rafi <imtiazrafi199918@gmail.com>
2022-01-23 23:41:13 +0600

    project will be in a page

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <div className="body">
+                    <h1>My Dummy Projects</h1>
+                </div>
+            </>
+        );
+    }
+}
+
+export default App;
